7b695f7e9f525f17333f0e33edec4768336dcd12
Leo <leonardo.groome@gmail.com>
2024-09-14 12:22:14 -0600

    Added Caledar to the Vue App

--- frontend/app/src/components/Calendar.vue
+++ frontend/app/src/components/Calendar.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <h1 class="pt-6 pl-6 text-4xl text-black border-b border-gray-300">
+  <div class="w-fit" id="screen">
+    <h1 class="pt-6 pl-6 mb-6 text-4xl text-black border-b border-gray-300">
       Calendar
     </h1>
+    <div id="calendar" class="flex flex-col ml-3">
+      <h2 class="text-3xl text-black">Task Calendar</h2>
+      <vue-cal
+        style="height: 32rem"
+        default-view="week"
+        :time="true"
+        :weekdays="[1, 2, 3, 4, 5, 6, 0]"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
 export default {
   name: "Calendar",
+  components: {
+    VueCal, // Register the Vue Cal component
+  },
 };
 </script>
--- frontend/app/src/components/Home.vue
+++ frontend/app/src/components/Home.vue
@@ -3,6 +3,19 @@
     <h1 class="pt-6 pl-6 mb-6 text-4xl text-black border-b border-gray-300">
       Home
     </h1>
+    <div class="flex flex-row justify-center items-end mb-4" id="addTask">
+      <input
+        v-model="newTask"
+        class="flex p-2 rounded-md border border-gray-300"
+        placeholder="New task"
+      />
+      <button
+        @click="addTask"
+        class="flex p-2 ml-2 text-white bg-blue-500 rounded-md"
+      >
+        Add Task
+      </button>
+    </div>
     <div id="Tasks" class="flex flex-row">
       <div
         class="flex-col items-center m-1 w-1/3 bg-white border-r border-gray-300"
